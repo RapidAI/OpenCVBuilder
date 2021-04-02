ebd6a9d3d52f910f7923f8525766cbf54cae65c3
benjaminwan <benjaminwan@163.com>
2021-04-02 16:54:36 +0800

    add github workflow config

--- build-opencv4.sh
+++ build-opencv4.sh
@@ -1,12 +1,27 @@
 #!/usr/bin/env bash
 # build opencv 4.5.x base on https://github.com/nihui/opencv-mobile/blob/master/opencv4_cmake_options.txt
 
-function cmakeParams() {
+function cmakeParamsMac() {
   mkdir -p "build-$1"
   pushd "build-$1"
   cmake -DCMAKE_BUILD_TYPE=$1 -DCMAKE_CONFIGURATION_TYPES=$1 \
     -DCMAKE_INSTALL_PREFIX=install \
-    $OMP_FLAG \
+    -DOpenMP_C_FLAGS="-Xpreprocessor -fopenmp -I/usr/local/opt/libomp/include" \
+    -DOpenMP_CXX_FLAGS="-Xpreprocessor -fopenmp -I/usr/local/opt/libomp/include" \
+    -DOpenMP_CXX_LIB_NAMES="omp" \
+    -DOpenMP_C_LIB_NAMES="omp" \
+    $(cat ../opencv4_cmake_options.txt) \
+    ..
+  cmake --build . -j $NUM_THREADS
+  cmake --build . --target install
+  popd
+}
+
+function cmakeParamsLinux() {
+  mkdir -p "build-$1"
+  pushd "build-$1"
+  cmake -DCMAKE_BUILD_TYPE=$1 -DCMAKE_CONFIGURATION_TYPES=$1 \
+    -DCMAKE_INSTALL_PREFIX=install \
     $(cat ../opencv4_cmake_options.txt) \
     ..
   cmake --build . -j $NUM_THREADS
@@ -17,21 +32,14 @@ function cmakeParams() {
 sysOS=$(uname -s)
 NUM_THREADS=1
 
-if [ -f /usr/local/opt/libomp/include/omp.h ]; then
-  OMP_FLAG='-DOpenMP_C_FLAGS="-Xpreprocessor -fopenmp -I/usr/local/opt/libomp/include"
-   -DOpenMP_CXX_FLAGS="-Xpreprocessor -fopenmp -I/usr/local/opt/libomp/include"
-   -DOpenMP_CXX_LIB_NAMES="omp"
-   -DOpenMP_C_LIB_NAMES="omp"'
-fi
-
 if [ $sysOS == "Darwin" ]; then
   #echo "I'm MacOS"
   NUM_THREADS=$(sysctl -n hw.ncpu)
+  cmakeParamsMac "Release"
 elif [ $sysOS == "Linux" ]; then
   #echo "I'm Linux"
   NUM_THREADS=$(grep ^processor /proc/cpuinfo | wc -l)
+  cmakeParamsLinux "Release"
 else
   echo "Other OS: $sysOS"
 fi
-
-cmakeParams "Release"
