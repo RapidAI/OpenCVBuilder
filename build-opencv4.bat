91f118accf394a5441a1c138a756ec4da0beafd6
benjaminwan <benjaminwan@163.com>
2021-04-03 10:54:15 +0800

    add windows build script

---
+++ build-opencv4.bat
@@ -0,0 +1,29 @@
+:: build opencv 4.5.x for windows by benjaminwan
+@ECHO OFF
+chcp 65001
+cls
+SETLOCAL EnableDelayedExpansion
+
+for /f "Delims=" %%x in (opencv4_cmake_options.txt) do set OPTIONS=!OPTIONS!%%x 
+
+::call :cmakeParams "Visual Studio 14 2015" "v140" "x64"
+::call :cmakeParams "Visual Studio 14 2015" "v140" "Win32"
+
+call :cmakeParams "Visual Studio 15 2017" "v141" "x64"
+::call :cmakeParams "Visual Studio 15 2017" "v141" "Win32"
+
+::call :cmakeParams "Visual Studio 16 2019" "v142" "x64"
+::call :cmakeParams "Visual Studio 16 2019" "v142" "Win32"
+GOTO:EOF
+
+:cmakeParams
+mkdir "build-%~2-%~3"
+pushd "build-%~2-%~3"
+cmake -G "%~1" -T "%~2,host=x64" -A "%~3" ^
+  -DCMAKE_BUILD_TYPE=Release -DCMAKE_CONFIGURATION_TYPES=Release ^
+  %OPTIONS% ^
+  ..
+cmake --build . --config Release -j %NUMBER_OF_PROCESSORS%
+cmake --build . --config Release --target install
+popd
+GOTO:EOF
