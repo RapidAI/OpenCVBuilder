b464f8d15ba1023a6774b50f30b90461839fab12
benjaminwan <benjaminwan@163.com>
2024-01-01 10:29:37 +0800

    fix linux builder

--- build-opencv4.sh
+++ build-opencv4.sh
@@ -1,15 +1,30 @@
 #!/usr/bin/env bash
-# build opencv 4.6.x base on https://github.com/nihui/opencv-mobile/blob/master/opencv4_cmake_options.txt
+# build opencv 4.x base on https://github.com/nihui/opencv-mobile/blob/master/opencv4_cmake_options.txt
 
-function cmakeParams() {
+function cmakeBuild() {
   mkdir -p "build-$1"
   pushd "build-$1"
-  cmake -DCMAKE_BUILD_TYPE=$1 -DCMAKE_CONFIGURATION_TYPES=$1 \
+  cmake -DCMAKE_BUILD_TYPE=$1 \
+    -DCMAKE_CONFIGURATION_TYPES=$1 \
     -DCMAKE_INSTALL_PREFIX=install \
     $(cat ../opencv4_cmake_options.txt) \
     ..
-  cmake --build . --config $1 -j $NUM_THREADS
-  cmake --build . --config $1 --target install
+  cmake --build . -j $NUM_THREADS
+  cmake --build . --target install
+  popd
+}
+
+function cmakeCrossBuild() {
+  mkdir -p "build-$1"
+  pushd "build-$1"
+  cmake -DCMAKE_TOOLCHAIN_FILE=../musl-cross.toolchain.cmake \
+    -DCMAKE_BUILD_TYPE=$1 \
+    -DCMAKE_CONFIGURATION_TYPES=$1 \
+    -DCMAKE_INSTALL_PREFIX=install \
+    $(cat ../opencv4_cmake_options.txt) \
+    ..
+  cmake --build . -j $NUM_THREADS
+  cmake --build . --target install
   popd
 }
 
@@ -19,24 +34,21 @@ NUM_THREADS=1
 if [ $sysOS == "Darwin" ]; then
   #echo "I'm MacOS"
   NUM_THREADS=$(sysctl -n hw.ncpu)
-  cmakeParams "Release"
+  cmakeBuild "Release"
 elif [ $sysOS == "Linux" ]; then
   #echo "I'm Linux"
   NUM_THREADS=$(grep ^processor /proc/cpuinfo | wc -l)
-  if [ "$1" ]; then
-    echo "CC=$1"
-    export CC="$1"
+  if [ "$1" ] && [ "$2" ]; then
+    echo "TOOLCHAIN_NAME=$1"
+    echo "TOOLCHAIN_PATH=$2"
+    export TOOLCHAIN_NAME="$1"
+    export TOOLCHAIN_PATH="$2"
+    echo "cross build"
+    cmakeCrossBuild "Release"
   else
-    echo "$1 CC is empty, use gcc"
+    echo "native build"
+    cmakeBuild "Release"
   fi
-
-  if [ "$2" ]; then
-   echo "CXX=$2"
-    export CXX="$2"
-  else
-    echo "$2 CXX is empty, use g++"
-  fi
-  cmakeParams "Release"
 else
   echo "Other OS: $sysOS"
 fi
