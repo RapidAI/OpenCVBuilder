b79463c97fc3f0176556d6014005a96629e794e0
benjaminwan <benjaminwan@163.com>
2024-07-19 17:44:51 +0800

    update all builder

--- build-opencv4-mac.sh
+++ build-opencv4-mac.sh
@@ -1,26 +1,56 @@
 #!/usr/bin/env bash
-# ./build-opencv4-mac.sh "x86_64"
-# ./build-opencv4-mac.sh "arm64"
+# ./build-opencv4-mac.sh -n x86_64
+# ./build-opencv4-mac.sh -n arm64
 
-if [ "$1" ]; then
-  echo "TARGET_ARCH=$1"
-  TARGET_ARCH="$1"
+HOST_OS=$(uname -s)
+NUM_THREADS=1
+BUILD_TYPE=Release
+
+if [ "$HOST_OS" == "Darwin" ]; then
+    NUM_THREADS=$(sysctl -n hw.ncpu)
 else
-  echo "TARGET_ARCH use default x86_64"
-  TARGET_ARCH="x86_64"
+    echo "Unsupported OS: $HOST_OS"
+    exit 0
 fi
 
-NUM_THREADS=$(sysctl -n hw.ncpu)
-BUILD_TYPE="Release"
+JAVA_FLAG=""
+TARGET_ARCH=""
 
-mkdir -p "build-$BUILD_TYPE-$TARGET_ARCH"
-pushd "build-$BUILD_TYPE-$TARGET_ARCH" || exit
-cmake -DCMAKE_SYSTEM_NAME="Darwin" -DCMAKE_SYSTEM_PROCESSOR="$TARGET_ARCH" -DCMAKE_OSX_ARCHITECTURES="$TARGET_ARCH" \
-  -DCMAKE_OSX_DEPLOYMENT_TARGET="10.9" \
-  -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_CONFIGURATION_TYPES=$BUILD_TYPE \
-  -DCMAKE_INSTALL_PREFIX=install \
-  $(cat ../opencv4_cmake_options.txt) \
-    ..
-cmake --build . --config $BUILD_TYPE -j $NUM_THREADS
-cmake --build . --config $BUILD_TYPE --target install
-popd
+while getopts "n:j" arg; do
+    case $arg in
+    n)
+        echo "n(TARGET_ARCH):$OPTARG"
+        TARGET_ARCH="$OPTARG"
+        ;;
+    j)
+        echo "j's arg:$OPTARG"
+        JAVA_FLAG="-DBUILD_FAT_JAVA_LIB=ON -DBUILD_JAVA=ON -DBUILD_opencv_java=ON -DBUILD_opencv_flann=ON"
+        ;;
+    ?)
+        echo -e "unkonw argument."
+        exit 1
+        ;;
+    esac
+done
+
+if [ -z "$TARGET_ARCH" ]; then
+    echo -e "empty TARGET_ARCH."
+    echo -e "usage1: ./build-opencv3-linux.sh -n x86_64"
+    echo -e "usage2: ./build-opencv3-linux.sh -n arm64"
+    exit 1
+fi
+
+BUILD_OUTPUT_PATH="build-$BUILD_TYPE-$TARGET_ARCH"
+BUILD_INSTALL_PATH="$BUILD_OUTPUT_PATH/install"
+mkdir -p "$BUILD_OUTPUT_PATH"
+
+cmake --compile-no-warning-as-error \
+    -B"$BUILD_OUTPUT_PATH" \
+    -DCMAKE_INSTALL_PREFIX="$BUILD_INSTALL_PATH" \
+    -DCMAKE_SYSTEM_NAME="Darwin" -DCMAKE_OSX_DEPLOYMENT_TARGET="10.9" \
+    -DCMAKE_SYSTEM_PROCESSOR="$TARGET_ARCH" -DCMAKE_OSX_ARCHITECTURES="$TARGET_ARCH" \
+    -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_CONFIGURATION_TYPES=$BUILD_TYPE \
+    $(cat opencv4_cmake_options.txt) \
+    $JAVA_FLAG
+
+cmake --build $BUILD_OUTPUT_PATH --config $BUILD_TYPE --parallel $NUM_THREADS --target install
--- build-opencv4-musl-gcc.sh
+++ build-opencv4-musl-gcc.sh
@@ -1,84 +1,51 @@
 #!/usr/bin/env bash
-#./build-opencv4-musl-gcc.sh "aarch64-linux-musl" "/opt/aarch64-linux-musl"
-#./build-opencv4-musl-gcc.sh "aarch64_be-linux-musl" "/opt/aarch64_be-linux-musl"
-#./build-opencv4-musl-gcc.sh "arm-linux-musleabi" "/opt/arm-linux-musleabi"
-#./build-opencv4-musl-gcc.sh "arm-linux-musleabihf" "/opt/arm-linux-musleabihf"
-#./build-opencv4-musl-gcc.sh "armeb-linux-musleabi" "/opt/armeb-linux-musleabi"
-#./build-opencv4-musl-gcc.sh "armeb-linux-musleabihf" "/opt/armeb-linux-musleabihf"
-#./build-opencv4-musl-gcc.sh "armel-linux-musleabi" "/opt/armel-linux-musleabi"
-#./build-opencv4-musl-gcc.sh "armel-linux-musleabihf" "/opt/armel-linux-musleabihf"
-#./build-opencv4-musl-gcc.sh "armv5l-linux-musleabi" "/opt/armv5l-linux-musleabi"
-#./build-opencv4-musl-gcc.sh "armv5l-linux-musleabihf" "/opt/armv5l-linux-musleabihf"
-#./build-opencv4-musl-gcc.sh "armv6-linux-musleabi" "/opt/armv6-linux-musleabi"
-#./build-opencv4-musl-gcc.sh "armv6-linux-musleabihf" "/opt/armv6-linux-musleabihf"
-#./build-opencv4-musl-gcc.sh "armv7l-linux-musleabihf" "/opt/armv7l-linux-musleabihf"
-#./build-opencv4-musl-gcc.sh "armv7m-linux-musleabi" "/opt/armv7m-linux-musleabi"
-#./build-opencv4-musl-gcc.sh "armv7r-linux-musleabihf" "/opt/armv7r-linux-musleabihf"
-#./build-opencv4-musl-gcc.sh "i486-linux-musl" "/opt/i486-linux-musl"
-#./build-opencv4-musl-gcc.sh "i686-linux-musl" "/opt/i686-linux-musl"
-#./build-opencv4-musl-gcc.sh "m68k-linux-musl" "/opt/m68k-linux-musl"
-#./build-opencv4-musl-gcc.sh "microblaze-linux-musl" "/opt/microblaze-linux-musl"
-#./build-opencv4-musl-gcc.sh "microblazeel-linux-musl" "/opt/microblazeel-linux-musl"
-#./build-opencv4-musl-gcc.sh "mips-linux-musl" "/opt/mips-linux-musl"
-#./build-opencv4-musl-gcc.sh "mips-linux-muslsf" "/opt/mips-linux-muslsf"
-#./build-opencv4-musl-gcc.sh "mips-linux-musln32sf" "/opt/mips-linux-musln32sf"
-#./build-opencv4-musl-gcc.sh "mips64-linux-musl" "/opt/mips64-linux-musl"
-#./build-opencv4-musl-gcc.sh "mips64-linux-musln32" "/opt/mips64-linux-musln32"
-#./build-opencv4-musl-gcc.sh "mips64-linux-musln32sf" "/opt/mips64-linux-musln32sf"
-#./build-opencv4-musl-gcc.sh "mips64el-linux-musl" "/opt/mips64el-linux-musl"
-#./build-opencv4-musl-gcc.sh "mips64el-linux-musln32" "/opt/mips64el-linux-musln32"
-#./build-opencv4-musl-gcc.sh "mips64el-linux-musln32sf" "/opt/mips64el-linux-musln32sf"
-#./build-opencv4-musl-gcc.sh "mipsel-linux-musl" "/opt/mipsel-linux-musl"
-#./build-opencv4-musl-gcc.sh "mipsel-linux-musln32" "/opt/mipsel-linux-musln32"
-#./build-opencv4-musl-gcc.sh "mipsel-linux-musln32sf" "/opt/mipsel-linux-musln32sf"
-#./build-opencv4-musl-gcc.sh "mipsel-linux-muslsf" "/opt/mipsel-linux-muslsf"
-#./build-opencv4-musl-gcc.sh "or1k-linux-musl" "/opt/or1k-linux-musl"
-#./build-opencv4-musl-gcc.sh "powerpc-linux-musl" "/opt/powerpc-linux-musl"
-#./build-opencv4-musl-gcc.sh "powerpc-linux-muslsf" "/opt/powerpc-linux-muslsf"
-#./build-opencv4-musl-gcc.sh "powerpc64-linux-musl" "/opt/powerpc64-linux-musl"
-#./build-opencv4-musl-gcc.sh "powerpc64le-linux-musl" "/opt/powerpc64le-linux-musl"
-#./build-opencv4-musl-gcc.sh "powerpcle-linux-musl" "/opt/powerpcle-linux-musl"
-#./build-opencv4-musl-gcc.sh "powerpcle-linux-muslsf" "/opt/powerpcle-linux-muslsf"
-#./build-opencv4-musl-gcc.sh "riscv32-linux-musl" "/opt/riscv32-linux-musl"
-#./build-opencv4-musl-gcc.sh "riscv64-linux-musl" "/opt/riscv64-linux-musl"
-#./build-opencv4-musl-gcc.sh "s390x-linux-musl" "/opt/s390x-linux-musl"
-#./build-opencv4-musl-gcc.sh "sh2-linux-musl" "/opt/sh2-linux-musl"
-#./build-opencv4-musl-gcc.sh "sh2-linux-muslfdpic" "/opt/sh2-linux-muslfdpic"
-#./build-opencv4-musl-gcc.sh "sh2eb-linux-musl" "/opt/sh2eb-linux-musl"
-#./build-opencv4-musl-gcc.sh "sh2eb-linux-muslfdpic" "/opt/sh2eb-linux-muslfdpic"
-#./build-opencv4-musl-gcc.sh "sh4-linux-musl" "/opt/sh4-linux-musl"
-#./build-opencv4-musl-gcc.sh "sh4eb-linux-musl" "/opt/sh4eb-linux-musl"
-#./build-opencv4-musl-gcc.sh "x86_64-linux-musl" "/opt/x86_64-linux-musl"
-#./build-opencv4-musl-gcc.sh "x86_64-linux-muslx32" "/opt/x86_64-linux-muslx32"
-
-if [ "$1" ]; then
-  echo "TOOLCHAIN_NAME=$1"
-  export TOOLCHAIN_NAME="$1"
-else
-  echo "Default toolchain name: x86_64-linux-musl"
-  export TOOLCHAIN_NAME="x86_64-linux-musl"
-fi
-
-if [ "$2" ] ; then
-  echo "TOOLCHAIN_PATH=$2"
-  export TOOLCHAIN_PATH=$2
-else
-  echo "Error: Must input a path to toolchain!"
-  exit
-fi
 
 HOST_OS=$(uname -s)
-NUM_THREADS=$(grep ^processor /proc/cpuinfo | wc -l)
-BUILD_TYPE="Release"
+NUM_THREADS=1
+BUILD_TYPE=Release
 
-mkdir -p "build-$BUILD_TYPE-$TOOLCHAIN_NAME"
-pushd "build-$BUILD_TYPE-$TOOLCHAIN_NAME" || exit
+if [ $HOST_OS == "Linux" ]; then
+    NUM_THREADS=$(nproc)
+else
+    echo "Unsupported OS: $HOST_OS"
+    exit 0
+fi
 
-cmake -DCMAKE_TOOLCHAIN_FILE=../musl-cross.toolchain.cmake \
-  -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_CONFIGURATION_TYPES=$BUILD_TYPE \
-  -DCMAKE_INSTALL_PREFIX=install \
-  $(cat ../opencv4_cmake_options.txt) \
-    ..
-cmake --build . --config $BUILD_TYPE -j $NUM_THREADS
-cmake --build . --config $BUILD_TYPE --target install
-popd
+while getopts "n:p:" arg; do
+    case $arg in
+    n)
+        echo "n's arg:$OPTARG"
+        export TOOLCHAIN_NAME="$OPTARG"
+        ;;
+    p)
+        echo "p's arg:$OPTARG"
+        export TOOLCHAIN_PATH="$OPTARG"
+        ;;
+    ?)
+        echo -e "unkonw argument."
+        exit 1
+        ;;
+    esac
+done
+echo "TOOLCHAIN_NAME=$TOOLCHAIN_NAME, TOOLCHAIN_PATH=$TOOLCHAIN_PATH"
+
+if [ -z "$TOOLCHAIN_NAME" ] || [ -z "$TOOLCHAIN_PATH" ]; then
+    echo -e "empty TOOLCHAIN_NAME or TOOLCHAIN_PATH."
+    echo -e "usage: ./build-opencv3-musl-gcc.sh -n 'aarch64-linux-musl' -p '/opt/aarch64-linux-musl'"
+    exit 1
+fi
+
+export PATH=$TOOLCHAIN_PATH/bin:$PATH
+
+BUILD_OUTPUT_PATH="build-$BUILD_TYPE-$TOOLCHAIN_NAME"
+BUILD_INSTALL_PATH="$BUILD_OUTPUT_PATH/install"
+
+mkdir -p "$BUILD_OUTPUT_PATH"
+cmake --compile-no-warning-as-error \
+    -B"$BUILD_OUTPUT_PATH" \
+    -DCMAKE_TOOLCHAIN_FILE=musl-cross.toolchain.cmake \
+    -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_CONFIGURATION_TYPES=$BUILD_TYPE \
+    -DCMAKE_INSTALL_PREFIX="$BUILD_INSTALL_PATH" \
+    $(cat opencv4_cmake_options.txt)
+
+cmake --build $BUILD_OUTPUT_PATH --config $BUILD_TYPE --parallel $NUM_THREADS --target install
